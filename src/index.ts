5bcd9b75eae2ea44e6241cdec25f1e8d28d83161
João Vitor Fernandes <jv.fernandes.a.s@gmal.com>
2024-04-23 23:10:02 -0300

    Feat: bot and messages initialization

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import { Telegraf } from 'telegraf'
+import { MessageManager } from './messageManager/messageManager';
+import { registerMessages } from './messages/messages';
 
 const main = () => {
-    if(process.env.BOT_TOKEN === undefined) {
+    const BOT_TOKEN = process.env.BOT_TOKEN;
+    if(BOT_TOKEN === undefined) {
         throw new Error('Bot token not defined in enviroment');
     }
+
+    const bot = new Telegraf(BOT_TOKEN);
+
+    bot.on('message', (ctx) => {
+        console.log(ctx.text)
+        MessageManager.manager().executeMessageFunction(ctx);
+    })
+    registerMessages();
+
+    bot.launch();
 }
 
 main();
